d4fdea592041bd70fb1edc7b9b8bf4cc40c345d8
alex8023 <alex8023@yandex.ru>
2023-10-19 13:54:10 +0300

    add mongodb & users collection all work

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ const cookieParser = require('cookie-parser');
 const sessions = require('express-session');
 require('dotenv').config();
 const MongoDBStore = require('connect-mongodb-session')(sessions);
-
+const { MongoClient } = require('mongodb');
 const app = express();
+
+// const mongoClientPromise = MongoClient.connect(process.env.MONGO_URI);
+
+app.use(async (req, res, next) => {
+  try {
+    const client = await MongoClient.connect(process.env.MONGO_URI);
+    req.db = client.db('session-test'); // = await MongoClient.connect(process.env.MONGO_URI).db('session-test') - не работает
+    next();
+  } catch (err) {
+    console.error(err);
+    next();
+  }
+});
 
 app.use(
   sessions({
@@ -24,11 +37,13 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// let session = {};
+async function findUserByUsername(db, username) {
+  return db.collection('users').findOne({ username });
+}
 
 app.get('/', (req, res) => {
   console.log(req.session, req.sessionID);
-  if (req.session.userName) {
+  if (req.session.username) {
     res.sendFile(path.resolve('./pages/index.html'));
   } else {
     res.redirect('/login');
@@ -43,12 +58,12 @@ app.get('/login', (req, res) => {
   }
 });
 
-app.post('/api/login', (req, res) => {
-  if (req.body.username == 'q' && req.body.password == 'q') {
-    // session = req.session;
-    req.session.userName = req.body.username;
-    // req.session.sessionId = nanoid();
-    // res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
+app.post('/api/login', async (req, res) => {
+  const { username, password } = await findUserByUsername(req.db, req.body.username);
+
+  console.log({ username, password });
+  if (req.body.username === username && req.body.password === password) {
+    req.session.username = req.body.username;
     res.redirect('/');
   } else {
     res.send('Invalid username or password');
